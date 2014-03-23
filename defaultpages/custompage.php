81567435aa2905f09ae847603311b7f36395c6ee
Martin Stoppler <stoppe@stoppler.de>
2014-03-23 12:30:16 +0100

    enable [SITE_URL] in custompages

--- defaultpages/custompage.php
+++ defaultpages/custompage.php
@@ -2,7 +2,7 @@
 if (is_numeric(KOM::$active['custompageid'])) {
     $pages = KOM::$dblink->Select("custompages", "*", "WHERE `id`=".(int)KOM::$active['custompageid']);
     if (isset($pages[0])) {
-        echo $pages[0]->content;
+        echo str_replace("[SITE_URL]", KOM::$site_url, $pages[0]->content);
     }
 }
 
